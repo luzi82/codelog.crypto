aea59e829c593372322c74380153ebc59fdb9583
Luzi82 <luzi82@gmail.com>
2015-05-21 22:01:32 +0800

    add test case of pem file

--- ci.sh
+++ ci.sh
@@ -11,14 +11,29 @@ cd tmp
 
 # RSA + SHA256 sign
 
-# generate private key "key.pem"
+# generate private key "private.pem"
 openssl genpkey -algorithm RSA -out private.pem
 
-# convert "key.pem" to public key
+# convert "private.pem" to public key "public.pem"
 openssl rsa -in private.pem -out public.pem -outform PEM -pubout
 
 # sign with rsa sha256
-openssl dgst -sha256 -sign private.pem -binary -out data.jpg.sig data.jpg
+openssl dgst -sha256 -sign private.pem -binary -out data.jpg.pem.sig data.jpg
 
 # verify
-openssl dgst -sha256 -verify public.pem -signature data.jpg.sig data.jpg 
+openssl dgst -sha256 -verify public.pem -signature data.jpg.pem.sig data.jpg 
+
+# convert "private.pem" to "private.der"
+openssl rsa -in private.pem -out private.der -outform DER
+
+# convert "public.pem" to "public.der"
+openssl rsa -in public.pem -out public.der -outform DER -pubin -pubout
+
+# sign with rsa sha256 by der
+openssl dgst -sha256 -sign private.der -keyform DER -binary -out data.jpg.der.sig data.jpg
+
+# verify
+openssl dgst -sha256 -verify public.der -keyform DER -signature data.jpg.der.sig data.jpg 
+
+# diff sign
+diff data.jpg.pem.sig data.jpg.der.sig
